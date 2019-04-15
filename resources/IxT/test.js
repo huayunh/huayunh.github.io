5dff2f6a0955bdde26957a7892eb7b0d4e4edf96
huayunh <96_mouse@sina.com>
2019-04-15 02:23:36 -0400

    add pressure

--- resources/IxT/test.js
+++ resources/IxT/test.js
@@ -3,15 +3,9 @@ const sqrt2 = Math.sqrt(2);
 const s300 = 300 - 75 * sqrt2;
 const l300 = 300 + 75 * sqrt2;
 
-var sessionLog = [];
-
-
-// 0 = tracingLine task
-// 1 = connectingDots task
-var connectingDots = 0; 
-var currentRun = 0;
-const totalRuns = 2;
 const taskNames = ["tracing", "connecting"];
+const pointerCirleRadius = 80;
+const anchorPressure = 0.5;
 
 const tracingLines = [
 {
@@ -144,8 +138,29 @@ const tracingLines = [
 },
 ]
 
-// var currentTracingLine = tracingLines.length - 1;
+// number of line drawn in the current run
 var currentTracingLine = 0;
+// number of runs. One run = 8 lines
+var currentRun = 0;
+
+var sessionLog = [];
+
+// 0 = tracingLine task
+// 1 = connectingDots task
+var connectingDots = 0; 
+
+// total number of
+// if practice run, set the runs to 2
+
+var totalRuns; 
+var param = getUrlVars();
+if (param && param['runs']) {
+	// practice run
+	totalRuns = parseInt(param['runs']);
+} else {
+	// actual study
+	totalRuns = 10;
+}
 
 $(function (){
 
@@ -156,6 +171,8 @@ var userLine; // to store X, Y components
 var userDistance; // to store distance between user line and computed tracing line
 var startTime;
 var endTime;
+var currentPointerLocation;
+var currentPointerPressure;
 
 var $c = $("#myCanvas");
 
@@ -171,21 +188,6 @@ $c.mousedown((e) => {
 	startTime = e.timeStamp;
 });
 
-$c.mousemove((e) => {
-
-	// if experiment has ended
-	if (connectingDots == -1) {
-		return;
-	}
-
-	if (e.buttons >= 1){
-		var rect = c.getBoundingClientRect();
-		logCoordinate(e.clientX - rect.left, e.clientY - rect.top, e.timeStamp);
-		// console.log(e);
-		updateCanvas();
-	}
-});
-
 $c.mouseup((e) => {
 
 	// if experiment has ended
@@ -194,37 +196,63 @@ $c.mouseup((e) => {
 	}
 
 	endTime = e.timeStamp;
-	clearLog();
-	currentTracingLine = (currentTracingLine + 1) % tracingLines.length;
-	if (currentTracingLine == 0) {
-		currentRun += 1;
-		if (currentRun >= totalRuns) {
-			if (connectingDots == 0) {
-				// move on to the connecting dots task
-				connectingDots = 1;
-				currentRun = 0;
-				$('#hint-text')
-					.html('Please connect the dots using a straight line. Please draw from the blue dot.');
-			} else if (connectingDots == 1) {
-				// everything is finished, end of experiment
-				ctx.clearRect(0, 0, c.width, c.height);
-				ctx.fillStyle = "#ccc";
-				ctx.fillText("end", c.width/2, c.height/2);
-				connectingDots = -1;
+	if (clearLog()) {
+		currentTracingLine = (currentTracingLine + 1) % tracingLines.length;
+		if (currentTracingLine == 0) {
+			currentRun += 1;
+			if (currentRun >= totalRuns) {
+				if (connectingDots == 0) {
+					// move on to the connecting dots task
+					connectingDots = 1;
+					currentRun = 0;
+					$('#hint-text')
+						.html('Please connect the dots using a straight line. Please draw from the blue dot.');
+				} else if (connectingDots == 1) {
+					// everything is finished, end of experiment
+					ctx.clearRect(0, 0, c.width, c.height);
+					ctx.fillStyle = "#ccc";
+					ctx.fillText("end", c.width/2, c.height/2);
+					connectingDots = -1;
 
-				var today = new Date();
-				var dd = String(today.getDate()).padStart(2, '0');
-				var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-				var yyyy = today.getFullYear();
-				var hh = String(today.getHours()).padStart(2, '0');
-				var minute = String(today.getMinutes()).padStart(2, '0');
-				downloadObjectAsJson(sessionLog, yyyy+mm+dd+hh+minute);
-				return;
+					var today = new Date();
+					var dd = String(today.getDate()).padStart(2, '0');
+					var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+					var yyyy = today.getFullYear();
+					var hh = String(today.getHours()).padStart(2, '0');
+					var minute = String(today.getMinutes()).padStart(2, '0');
+					downloadObjectAsJson(sessionLog, yyyy+mm+dd+hh+minute);
+					return;
+				}
 			}
 		}
 	}
 	updateCanvas();
 });
+
+c.addEventListener('pointermove', function(e) {
+
+	// if experiment has ended
+	if (connectingDots == -1) {
+		return;
+	}
+
+	var rect = c.getBoundingClientRect();
+
+	var x = e.clientX - rect.left;
+	var y = e.clientY - rect.top;
+
+	if (e.buttons >= 1){
+		if (e.pointerType == "mouse") {
+			currentPointerLocation = [-1, -1];
+			logCoordinate(x, y, e.timeStamp, -1);
+		} else {
+			currentPointerLocation = [x, y];
+			currentPointerPressure = e.pressure;
+			logCoordinate(x, y, e.timeStamp, e.pressure);
+		}
+		updateCanvas();
+	}
+}, false);
 
 function updateCanvas(){
 	ctx.clearRect(0, 0, c.width, c.height);
@@ -245,6 +273,25 @@ function updateCanvas(){
 		ctx.beginPath();
 		ctx.arc(tracingLines[currentTracingLine].start[0], tracingLines[currentTracingLine].start[1], 3, 0, 2 * Math.PI);
 		ctx.fill();
+
+		if (currentPointerLocation[0] !== -1){
+
+			ctx.fillStyle = "#0005";
+			ctx.beginPath();
+			ctx.arc(currentPointerLocation[0], 
+				currentPointerLocation[1], 
+				pointerCirleRadius*currentPointerPressure, 0, 2 * Math.PI);
+			ctx.fill();
+
+			ctx.strokeStyle = "green";
+			ctx.beginPath();
+			ctx.arc(
+				currentPointerLocation[0], 
+				currentPointerLocation[1], 
+				pointerCirleRadius * anchorPressure, 0, 2 * Math.PI);
+			ctx.stroke();
+		}
+
 	} else if (connectingDots = 1) { 
 		ctx.fillStyle = "blue";
 		ctx.beginPath();
@@ -255,6 +302,25 @@ function updateCanvas(){
 		ctx.beginPath();
 		ctx.arc(tracingLines[currentTracingLine].end[0], tracingLines[currentTracingLine].end[1], 3, 0, 2 * Math.PI);
 		ctx.fill();
+
+		if (currentPointerLocation[0] !== -1){
+
+			ctx.fillStyle = "#0005";
+			ctx.beginPath();
+			ctx.arc(currentPointerLocation[0], 
+				currentPointerLocation[1], 
+				pointerCirleRadius*currentPointerPressure, 0, 2 * Math.PI);
+			ctx.fill();
+
+			ctx.strokeStyle = "green";
+			ctx.beginPath();
+			ctx.arc(
+				currentPointerLocation[0], 
+				currentPointerLocation[1], 
+				pointerCirleRadius * anchorPressure, 0, 2 * Math.PI);
+			ctx.stroke();
+		}
+
 	}
 
 	// draw the user lines
@@ -273,20 +339,22 @@ function calculateDistanceToTheTracingLine(userX, userY){
 	return tracingLines[currentTracingLine].distance(userX, userY);
 }
 
-function logCoordinate(x,y, time){
+function logCoordinate(x,y, time, pressure){
 	var distance = calculateDistanceToTheTracingLine(x, y);
 	userLine.push(
 		{
 			"x": x, 
 			"y": y, 
 			"time": time,
-			"distance": distance
+			"distance": distance,
+			"pressure": pressure
 		});
 	userDistance.push(distance);
 }
 
 function clearLog() {
-	if (userLine && userLine.length > 2) {
+
+	if (userLine && userLine.length > 15) {
 		var firstUserPointToTracingLineDistance = 
 			distanceBetweenPoints([userLine[0].x, 
 								   userLine[0].y], 
@@ -316,10 +384,23 @@ function clearLog() {
 		console.log("avg", averageDistance);
 		console.log("score", score);
 
+		startTime = -1;
+		endTime = -1;
+		userLine = [];
+		userDistance = [-1];
+		currentPointerLocation = [-1, -1];
+		currentPointerPressure = 0;
+		return true;
 	}
-	startTime, endTime = -1, -1;
+
+	// too short
+	startTime = -1;
+	endTime = -1;
 	userLine = [];
 	userDistance = [-1];
+	currentPointerLocation = [-1, -1];
+	currentPointerPressure = 0;
+	return false;
 }
 
 clearLog();
@@ -340,4 +421,13 @@ function downloadObjectAsJson(exportObj, exportName){
     document.body.appendChild(downloadAnchorNode); // required for firefox
     downloadAnchorNode.click();
     downloadAnchorNode.remove();
-  }
+}
+
+// https://html-online.com/articles/get-url-parameters-javascript/
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
