bd57e576102287024bf421a78ee2e0c717b583c5
huayunh <96_mouse@sina.com>
2019-05-31 21:05:58 -0400

    add more functionality to the stego project

--- resources/steganography/x.js
+++ resources/steganography/x.js
@@ -1,9 +1,10 @@
 
 // constants
 const categories = [
-	'bricks',
+'bricks',
 ]
 const numberOfPicturesInTheCategory = 3;
+const startingTime = new Date();
 
 // parameters
 var param = getUrlVars();
@@ -19,54 +20,113 @@ if (param) {
 	else { var category = Math.floor(Math.random()*categories.length); }
 	category = categories[category];
 
+	if (param['debug']) { var debug = true; }
+	else { var debug = false; }
+
 }
 
 var pairs = generatePermutationSequence(numberOfPicturesInTheCategory);
 
-var currentQuestion = 0
+var currentQuestion = 0;
 var totalQuestions = pairs.length;
+var currentSelection = -1;
+
+var log = {
+	startTime: startingTime.getTime(),
+	pairs: pairs,
+	selection: [],
+	reactionTime: [],
+	category: category,
+	userAgent: $.browser,
+}
+
+var comparisonBeginTime = -1;
 
 $(function (){
+
+	$("#introduction input").keypress(function () {
+		if ($("#introduction input").val().length > 4) {
+			$('#introduction .next-button a').addClass('display');
+			$('#introduction p').removeClass('hidden');
+		}
+	});
+
+	$('#introduction .next-button a').click((e) => {
+		log.MTurkID = $("#introduction input").val();
+		$('#introduction').addClass('hidden');
+		$('#main-test').removeClass('hidden');
+		comparisonBeginTime = e.timeStamp;
+	});
 
 	$('#total-problem').html("" + totalQuestions);
 
 	$('#image1').click(() => {
 		$('#image1').addClass('selected');
 		$('#image2').removeClass('selected');
-		$('#next-button a').addClass('display');
+		$('#main-test .next-button a').addClass('display');
+		currentSelection = 0;
 	});
 
 	$('#image2').click(() => {
 		$('#image2').addClass('selected');
 		$('#image1').removeClass('selected');
-		$('#next-button a').addClass('display');
+		$('#main-test .next-button a').addClass('display');
+		currentSelection = 1;
 	});
 
 	$('#progressbar')
-		.progressbar({
-			max: totalQuestions,
-			value: currentQuestion + 1
-		})
-		.progressbar( "enable" );
+	.progressbar({
+		max: totalQuestions,
+		value: currentQuestion + 1
+	})
+	.progressbar( "enable" );
+
+	$('input:text')
+		.button()
+		.css({
+		      'font' : 'inherit',
+		     'color' : 'inherit',
+		'text-align' : 'left',
+		   'outline' : 'none',
+		    'cursor' : 'text'
+		});
 
 	// preload every image
 
 	// update
 
-	$('#next-button a').click( () => {
-		if (!$('#next-button a').hasClass('display')) { return; }
+	$('#main-test .next-button a').click( (e) => {
+		if (!$('.next-button a').hasClass('display')) { return; }
+		log.selection.push(pairs[currentQuestion][currentSelection]);
+		log.reactionTime.push(e.timeStamp - comparisonBeginTime);
+
+		currentSelection = -1;
 		currentQuestion += 1;
-		console.log(currentQuestion)
-		updateImages();
+		if (currentQuestion == totalQuestions) {
+			$("#main-test").addClass('hidden');
+			$('#survey').removeClass('hidden');
+			if (debug) {
+				downloadObjectAsJson(log, category + "-" + log.startTime);
+			}
+		} else {
+			updateImages();
+			comparisonBeginTime = e.timeStamp;
+		}
 	});
 
 	function updateImages(){
-		$('#next-button a').removeClass('display');
+		$('#main-test.next-button a').removeClass('display');
 		$('#progressbar').progressbar("option", "value", currentQuestion);
 		$('#image1').css('background-image', 'url("images/' + category + '/' + (pairs[currentQuestion][0]+1) + '.jpg")')
 		$('#image2').css('background-image', 'url("images/' + category + '/' + (pairs[currentQuestion][1]+1) + '.jpg")')
 		$('#current-problem').html(currentQuestion + 1);
 		$('.images div').removeClass('selected');
+
+		if (debug){
+			$('#image1 span').html(pairs[currentQuestion][0]+1)
+			$('#image2 span').html(pairs[currentQuestion][1]+1)
+		}
+
 	}
 
 	updateImages();
@@ -77,11 +137,11 @@ $(function (){
 
 // https://html-online.com/articles/get-url-parameters-javascript/
 function getUrlVars() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-        vars[key] = value;
-    });
-    return vars;
+	var vars = {};
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+		vars[key] = value;
+	});
+	return vars;
 }
 
 function generatePermutationSequence(n) {
@@ -89,34 +149,58 @@ function generatePermutationSequence(n) {
 	var generatedList = [];
 	for (var i = 0; i < n; i++) {
 		for (var j = i + 1; j < n; j++) {
-			generatedList.push([i,j]);
-		}
-	}
-	for (var i = 0; i < n; i++) {
-		for (var j = i + 1; j < n; j++) {
-			generatedList.push([j,i]);
+			if ((!imagePairOrderingMatters) && imageOrderRandomized) {
+				if (Math.random() > 0.5) { generatedList.push([i,j]); }
+				else { generatedList.push([j,i]); }
+			} else {
+				generatedList.push([i,j]);
+			}
 		}
 	}
 
-	return generatedList;
+	if (imagePairOrderingMatters) {
+		for (var i = 0; i < n; i++) {
+			for (var j = i + 1; j < n; j++) {
+				generatedList.push([j,i]);
+			}
+		}
+	}
+
+	if (imageOrderRandomized){
+		return shuffle(generatedList);
+	} else {
+		return generatedList;
+	}
+
 }
 
 // https://stackoverflow.com/a/2450976
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+	var currentIndex = array.length, temporaryValue, randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
 
-  return array;
+	return array;
+}
+
+// https://stackoverflow.com/a/30800715
+function downloadObjectAsJson(exportObj, exportName){
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href",     dataStr);
+    downloadAnchorNode.setAttribute("download", exportName + ".json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
 }
